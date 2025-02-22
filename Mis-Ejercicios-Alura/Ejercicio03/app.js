76fe937790042d543e13b0c1c6bc9d4cf1f14305
jlescano-mobup <jlescano@mobuptacna.com>
2025-02-22 07:47:56 -0500

    Agrego break, limite de intentos, informe al usuario.

---
+++ Mis-Ejercicios-Alura/Ejercicio03/app.js
@@ -0,0 +1,23 @@
+
+// let contadorUno = 1;
+
+// while (contadorUno < 11) {
+//     console.log(contadorUno);
+//     contadorUno = contadorUno +1;
+// }
+
+// let contadorDos = 10;
+
+// while ( contadorDos >= 0 ) {
+//     console.log(contadorDos);
+//     contadorDos = contadorDos -1;
+    
+// }
+
+let numeroUsuario = prompt("Ingrese un numero para empezar la cuenta regresiva");
+let numeroInicial = 0
+while (numeroInicial <= numeroUsuario) {
+    console.log(numeroInicial);
+    numeroInicial = numeroInicial +1;
+    
+}
